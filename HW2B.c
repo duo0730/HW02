69551bac33cfa1d24627931d8fa2173d826095fd
朵朵 <411221259@gms.ndhu.edu.tw>
2023-11-04 16:41:06 +0800

    Update HW2B.c

--- HW2B.c
+++ HW2B.c
@@ -1,87 +1,74 @@
 #include <stdio.h>
-#define IN_CHESS(x,y)(x>=0 && x<=7 && y>=0 && y<=7)
 
-int main(){
-    int chess[8][8]={
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,2,1,0,0,0},
-        {0,0,0,1,2,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0}
+#define is_bound(x, y) (x >= 0 && x <= 7 && y >= 0 && y <= 7)
+
+int chess[8][8] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 1, 2, 0, 0, 0},
+    {0, 0, 0, 2, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+int check(int chess[8][8], int i, int j, int color) {
+    if (chess[i][j] != 0) {
+        return 0; // (i,j) 不可下 color 棋子
+    }
+
+    int directions[8][2] = {
+        {-1, 0}, {1, 0}, {0, -1}, {0, 1},
+        {-1, -1}, {1, 1}, {-1, 1}, {1, -1}
     };
-    
-    int color =1;
-    int i=2; 
-    int j=4;
-    int dx=1; 
-    int dy=0;
 
-    int check(int check[0][0])
-    int an_color=3-color;
-    
-    //step 1 可不可以下
-    int x=i+dx;
-    int y=j+dy;
+    int opp_color = (color == 1) ? 2 : 1; // 對手的顏色
 
-    if(IN_CHESS(x,y)){
-        if(chess[x][y]!=2){
-            printf("%d,%d not free for color=%d\n",i,j,color);
+    for (int k = 0; k < 8; k++) {
+        int dx = directions[k][0];
+        int dy = directions[k][1];
+        int x = i + dx;
+        int y = j + dy;
+        int can_place = 0;
+
+        while (is_bound(x, y)) {
+            if (chess[x][y] == 0) {
+                break;
+            } else if (chess[x][y] == opp_color) {
+                x += dx;
+                y += dy;
+            } else if (chess[x][y] == color) {
+                can_place = 1; // (i,j) 可以下 color 棋子
+                break;
+            }
         }
-    }else{
-        printf("%d,%d not free for color=%d\n",i,j,color);
-        return;
-    }
 
-    //chess[x][y]==2
-    x=x+dx;
-    y=y+dy;
-    while(IN_CHESS(x,y)){
-        if chess[x][y]==0{
-            printf("%d,%d not free for color=%d\n",i,j,color);
-            break;
-        }
-        if chess[x][y]==0{
-            printf("%d,%d free for color=%d\n",i,j,color);
-            break;
-        }
-        x=x+dx;
-        y=y+dy;
-    }
-
-
-    /*int color,i,j;
-    scanf("%d%d%d",&color,&i,&j);
-    
-    if(chess[i][j]!=0) printf("(%d,%d) 不能下 color=%d的子",i,j,color);
-
-
-    int vec[8][2]={
-        {0,1},{1,1},{1,0},{1,-1},
-        {0,-1},{-1,-1},{-1,0},{-1,1}
-    };
-     
-    {-1,-1} {-1, 0} {-1, 1}
-    { 0,-1}         { 0, 1}
-    { 1,-1} { 1, 0} { 1, 1}
-    
-
-    if(chess[i][j]==0){
-        for(int d=0;d<8;d++){
-            int dx=vec[d][0];
-            int dx=vec[d][1];
+        if (can_place) {
+            return 1;
         }
     }
 
-    if(chess[i][j]==0){
-    
-    }*/
-    
-    
     return 0;
 }
 
+int main() {
+    int i, j;
+    int color;
 
+    scanf("%d", &color);
+    scanf("%d %d", &i, &j);
+
+    if (chess[i][j] == 0) printf("(%d,%d) 為空白\n", i, j);
+    if (chess[i][j] == 1) printf("(%d,%d) 為黑子\n", i, j);
+    if (chess[i][j] == 2) printf("(%d,%d) 為白子\n", i, j);
     
+    int can_place = check(chess, i, j, color);
+    if (can_place) {
+        printf("(%d,%d) 能下color=%d的子\n", i, j,color);
+    } else {
+        printf("(%d,%d) 不能下color=%d的子\n", i, j,color);
+    }
+
+    return 0;
+}
